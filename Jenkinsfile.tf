81299e20fca90848ccb7b9d997492c1e10509baf
Lavinia-81 <131885710+Lavinia-81@users.noreply.github.com>
2024-08-24 14:02:23 +0100

    Update Jenkinsfile.tf

--- Jenkinsfile.tf
+++ Jenkinsfile.tf
@@ -15,7 +15,7 @@ pipeline {
                  script{
                         dir("terraform")
                         {
-                            git "https://github.com/yeshwanthlm/Terraform-Jenkins.git"
+                            git "https://github.com/Lavinia-81/Terraform-Jenkins/edit/main/Jenkinsfile.tf"
                         }
                     }
                 }
